dcb369ba3b4e4719cfb51a52c1a02d3ca129df83
AjaySirra <93808682+AjaySirra@users.noreply.github.com>
2021-12-16 00:57:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const path = require("path");
 const dbPath = path.join(__dirname, "cricketMatchDetails.db");
 const { open } = require("sqlite");
